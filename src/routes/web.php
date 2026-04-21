a7cf3e6fd59c01fcba9bae931b7bb2f5c0692fde
Frank <mobicardsystemslimited@gmail.com>
2026-04-21 10:37:39 +0300

    Update web routes to simplify only using web middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 use Illuminate\Support\Facades\Route;
 use MobicardApi\ScanApi\Http\Controllers\ScanApiController;
 
@@ -9,6 +9,22 @@ $middleware = config('scanapi.routes.middleware', ['web', 'scanapi.config']);
 Route::group([
     'prefix' => $prefix,
     'middleware' => $middleware
+], function () {
+    Route::get('scan', [ScanApiController::class, 'index'])->name('scanapi.scan');
+    Route::post('broker/scan', [ScanApiController::class, 'scanCard'])->name('scanapi.broker.scan');
+    Route::post('broker/upload', [ScanApiController::class, 'uploadCard'])->name('scanapi.broker.upload');
+});
+*/
+
+
+use Illuminate\Support\Facades\Route;
+use MobicardApi\ScanApi\Http\Controllers\ScanApiController;
+
+$prefix = config('scanapi.routes.prefix', 'mobicard');
+
+Route::group([
+    'prefix' => $prefix,
+    'middleware' => ['web'] // Only use web middleware
 ], function () {
     Route::get('scan', [ScanApiController::class, 'index'])->name('scanapi.scan');
     Route::post('broker/scan', [ScanApiController::class, 'scanCard'])->name('scanapi.broker.scan');
